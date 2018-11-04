9008a66b5a825d218d58a34bda8ab7bec3cb7237
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-04 20:32:06 -0300

    fix simbol test

--- models/simbol_test.go
+++ models/simbol_test.go
@@ -2,19 +2,19 @@ package models
 
 import "testing"
 
-func testGetSimbol(t *testing.T) {
+func TestGetSimbol(t *testing.T) {
 	s1, err := NewSimbol("r")
-	if err == nil || s1.IsVariable == true {
+	if err != nil || s1.IsVariable == true {
 		t.Error("Expected 'r' to not be a variable and not have errors")
 	}
 
 	s2, err := NewSimbol("R")
-	if err == nil || s2.IsVariable != true {
+	if err != nil || s2.IsVariable != true {
 		t.Error("Expected 'R' to be variable and not have errors")
 	}
 
 	s3, err := NewSimbol("Rar")
-	if err != nil || s3.Value != "" {
+	if err == nil || s3.Value != "" {
 		t.Error("Expected 'Rar' to have error")
 	}
 }
