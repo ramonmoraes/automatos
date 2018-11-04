27d7fd91c328259e32c3adecb38036bf936a3916
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-04 19:33:55 -0300

    add isEmpty option to simbol

--- models/simbol.go
+++ models/simbol.go
@@ -11,6 +11,7 @@ import (
 type Simbol struct {
 	Value      string
 	IsVariable bool
+	IsEmpty    bool
 }
 
 // NewSimbol should return a new instance of simbol
@@ -18,6 +19,14 @@ func NewSimbol(value string) (Simbol, error) {
 	if len(value) != 1 {
 		errorMessage := fmt.Sprintf("A Simbol '%s' can not have a length different from 1", value)
 		return Simbol{}, errors.New(errorMessage)
+	}
+
+	if value == "0" {
+		return Simbol{
+			Value:      value,
+			IsVariable: false,
+			IsEmpty:    true,
+		}, nil
 	}
 
 	return Simbol{
