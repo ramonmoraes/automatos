7ec76dade0b89215bd70b89bc184ed7edd52d55e
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-20 23:14:44 -0300

    emptyProductionV1Working

--- solver/emptyProductions.go
+++ solver/emptyProductions.go
@@ -1,6 +1,8 @@
 package solver
 
 import (
+	"fmt"
+
 	"../models"
 )
 
@@ -30,15 +32,34 @@ func RemoveEmptyProductions(automato models.Automato) models.Automato {
 	}
 
 	for emptyProductionVar, contentFromEmptyVar := range emptyProductions {
-		for _, wordsToBeAppendedWithEmptyVarContent := range atWithoutEmptyProductions.Expressions {
-			for _, word := range wordsToBeAppendedWithEmptyVarContent {
+		for expressionSimbol, wordsToBeAppendedWithEmptyVarContent := range atWithoutEmptyProductions.Expressions {
+			for i, word := range wordsToBeAppendedWithEmptyVarContent {
 				if word.Contains(emptyProductionVar) {
-					nw := word.ReplaceWords(emptyProductionVar, contentFromEmptyVar)
-					wordsToBeAppendedWithEmptyVarContent = append(wordsToBeAppendedWithEmptyVarContent, nw...)
+					newWords := word.ReplaceWords(emptyProductionVar, contentFromEmptyVar)
+					nonEmptyWords := []models.Word{}
+					for _, w := range newWords {
+						if !w.IsEmpty() {
+							nonEmptyWords = append(nonEmptyWords, w)
+						}
+					}
+
+					newWordsToBeAppended := []models.Word{}
+					newWordsToBeAppended = append(newWordsToBeAppended, wordsToBeAppendedWithEmptyVarContent[:i]...)
+					newWordsToBeAppended = append(newWordsToBeAppended, wordsToBeAppendedWithEmptyVarContent[i+1:]...)
+					newWordsToBeAppended = append(newWordsToBeAppended, nonEmptyWords...)
+					atWithoutEmptyProductions.Expressions[expressionSimbol] = newWordsToBeAppended
+					i = i + len(nonEmptyWords) - 1
 				}
 			}
 		}
 	}
 
 	return atWithoutEmptyProductions
+}
+
+func explain(words []models.Word) {
+	// fmt.Println("Explaining list of words")
+	for _, w := range words {
+		fmt.Println(w.ToString())
+	}
 }
--- solver/emptyProductions_test.go
+++ solver/emptyProductions_test.go
@@ -7,20 +7,48 @@ import (
 )
 
 func TestRemoveSimpleEmptyProduction(t *testing.T) {
-	at := models.NewAutomato([]string{"A -> a", "B -> 0"})
+	at := models.NewAutomato([]string{"A -> a | B", "B -> 0"})
 	atModified := RemoveEmptyProductions(at)
 	if len(atModified.Expressions) != 1 {
 		t.Error("Automato should have only one expression")
-		atModified.Explain()
 	}
+	atModified.Explain()
 }
 
 func TestRemoveDoubleEmptyProduction(t *testing.T) {
 	at := models.NewAutomato([]string{"A -> a|B", "B -> c | 0"})
 	atModified := RemoveEmptyProductions(at)
 
-	if len(atModified.Expressions) != 2 {
+	if len(atModified.Expressions) != 1 {
 		t.Error("Automato should have appended B content on A variable")
-		atModified.Explain()
 	}
+
+	for _, words := range atModified.Expressions {
+		if len(words) != 2 {
+			t.Error("Automato should have generated two words")
+		}
+		if words[0].ToString() != "a" || words[1].ToString() != "c" {
+			t.Error("Automato should have words 'a' and 'c'")
+		}
+	}
+
+	atModified.Explain()
+}
+
+func TestRemoveComplex(t *testing.T) {
+	at := models.NewAutomato([]string{"A -> a|aC", "C -> c | 0"})
+	atModified := RemoveEmptyProductions(at)
+	if len(atModified.Expressions) != 1 {
+		t.Error("Automato should have appended B content on A variable")
+	}
+
+	for _, words := range atModified.Expressions {
+		if len(words) != 2 {
+			t.Error("Automato should have generated two words")
+		}
+		if words[0].ToString() != "a" || words[1].ToString() != "ac" {
+			t.Error("Automato should have words 'a' and 'c'")
+		}
+	}
+	atModified.Explain()
 }
