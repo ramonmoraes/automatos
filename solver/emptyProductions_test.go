71276c015f4626f79760fc4a2b5af23f03ba7f0f
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-21 00:24:36 -0300

    empty productions done

--- solver/emptyProductions_test.go
+++ solver/emptyProductions_test.go
@@ -36,19 +36,31 @@ func TestRemoveDoubleEmptyProduction(t *testing.T) {
 }
 
 func TestRemoveComplex(t *testing.T) {
-	at := models.NewAutomato([]string{"A -> a|aC", "C -> c | 0"})
+	at := models.NewAutomato([]string{
+		"A -> B | aC",
+		"B -> d | C",
+		"C -> c | 0",
+	})
 	atModified := RemoveEmptyProductions(at)
-	if len(atModified.Expressions) != 1 {
-		t.Error("Automato should have appended B content on A variable")
+	if len(atModified.Expressions) != 2 {
+		t.Error("Automato should have two expressions")
 	}
 
-	for _, words := range atModified.Expressions {
-		if len(words) != 2 {
-			t.Error("Automato should have generated two words")
+	for Simbol, words := range atModified.Expressions {
+		if Simbol.Value == "A" {
+			if words[0].ToString() != "a" &&
+				words[1].ToString() != "B" &&
+				words[1].ToString() != "ac" {
+				t.Error("Automato should have words 'a' and 'B' and 'ac'")
+				atModified.Explain()
+			}
 		}
-		if words[0].ToString() != "a" || words[1].ToString() != "ac" {
-			t.Error("Automato should have words 'a' and 'c'")
+		if Simbol.Value == "B" {
+			if words[0].ToString() != "d" &&
+				words[1].ToString() != "c" {
+				t.Error("Automato should have words 'c' and 'd'")
+				atModified.Explain()
+			}
 		}
 	}
-	atModified.Explain()
 }
