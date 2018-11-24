24705c045b6f5576d4d392d4dc3337625247ba9b
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-24 19:05:03 -0300

    add another way of "calling" solver methods

--- solver/solver.go
+++ solver/solver.go
@@ -11,11 +11,11 @@ func Solve(a models.Automato) models.Automato {
 
 // Chomsky should return a simplified automato
 func Chomsky(a models.Automato) models.Automato {
-	at := RemoveEmptyProductions(a)
+	at := RemoveEmptyProductions(a) // null productions
 	// fmt.Println("[After Removing Empty Productions]")
 	// at.Explain()
 
-	at = UselessSimbol(at)
+	at = UselessSimbol(at) // unit productions
 	// fmt.Println("[After removing useless simbol]")
 	// at.Explain()
 
