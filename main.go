4128157009a297a4827ef782fee32dc8d78e9a8c
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-25 19:08:56 -0300

    just examples

--- main.go
+++ main.go
@@ -8,16 +8,22 @@ import (
 )
 
 func main() {
+	// at := models.NewAutomato([]string{
+	// 	"S -> ASA | aB ",
+	// 	"A -> B | S",
+	// 	"B -> b | 0",
+	// })
+
 	at := models.NewAutomato([]string{
-		"S -> ASA | aB ",
-		"A -> B | S",
-		"B -> b | 0",
+		"S -> aX | bY | b",
+		"X -> x | 0 | d",
+		"Y -> y | 0",
 	})
-	// at := models.NewAutomato([]string{"A -> aB | a", "B -> C | 0", "C -> D", "D -> ff"})
+
 	fmt.Println("[Initial Automato]")
 	at.Explain()
 
-	chomAt := solver.Solve(at)
-	// fmt.Println("[End Automato]")
+	chomAt := solver.Solve(at, false)
+	fmt.Println("[End Automato]")
 	chomAt.Explain()
 }
