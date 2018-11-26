552aa4fe6da613b21799f74edfdcf216bbbd94d0
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-26 16:19:52 -0200

    Complete main, with flags and input

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"os"
 
 	"./models"
 	"./solver"
@@ -9,22 +12,46 @@ import (
 
 func main() {
 	print("\033[H\033[2J") // clean console
-	// at := models.NewAutomato([]string{
-	// 	"S -> ASA | aB ",
-	// 	"A -> B | S",
-	// 	"B -> b | 0",
-	// })
+	runExample := flag.Bool("example", false, "Run example func instead of given inputs")
+	verbose := flag.Bool("verbose", false, "When true, it outputs every step of the algorithm")
+	flag.Parse()
 
+	if *runExample {
+		example(*verbose)
+	} else {
+		var atExpressions []string
+		var inputText string
+		fmt.Println("Write expressions:")
+
+		for inputText != "\n" {
+			reader := bufio.NewReader(os.Stdin)
+			inputText, _ = reader.ReadString('\n')
+			if inputText != "\n" {
+				atExpressions = append(atExpressions, inputText)
+			}
+		}
+
+		fmt.Println("[Initial Automato]")
+		at := models.NewAutomato(atExpressions)
+		at.Explain()
+
+		fmt.Println("[End Automato]")
+		chomAt := solver.Solve(at, *verbose)
+		chomAt.Explain()
+	}
+}
+
+func example(verbose bool) {
 	at := models.NewAutomato([]string{
-		"A -> xB | B",
-		"B -> fC | 0",
-		"C -> C | g | 0",
+		"S -> ASA | aB ",
+		"A -> B | S",
+		"B -> b | 0",
 	})
 
 	fmt.Println("[Initial Automato]")
 	at.Explain()
 
-	chomAt := solver.Solve(at, false)
+	chomAt := solver.Solve(at, verbose)
 	fmt.Println("[End Automato]")
 	chomAt.Explain()
 }
