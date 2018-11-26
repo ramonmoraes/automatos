338c51d01388b24cc0c85efdfdf097b013aeb5a2
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-26 13:41:57 -0200

    automato's GetVariables also getting variables that generates expressions

--- models/automatoUtils.go
+++ models/automatoUtils.go
@@ -3,10 +3,11 @@ package models
 // GetVariableList should return the list of every possible variables on automato
 func (a *Automato) GetVariableList() []Simbol {
 	varList := []Simbol{}
-	for _, words := range a.Expressions {
+	for s, words := range a.Expressions {
 		for _, word := range words {
 			varList = append(varList, word.GetVariables()...)
 		}
+		varList = append(varList, s)
 	}
 	return varList
 }
