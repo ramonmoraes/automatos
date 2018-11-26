32aa8c1a5fa7f43e9de79674b96e6c141544dc57
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-26 13:43:03 -0200

    removing single recursive as first solver step

--- solver/solver.go
+++ solver/solver.go
@@ -13,7 +13,7 @@ func Solve(a models.Automato, verbose bool) models.Automato {
 
 // Chomsky should return a simplified automato
 func Chomsky(a models.Automato, verbose bool) models.Automato {
-	at := RemoveEmptyProductions(a) // null productions
+	at := RemoveEmptyProductions(removeSingleRecursive(a)) // null productions
 	if verbose {
 		fmt.Println("[After Removing Empty/Null Productions]")
 		at.Explain()
