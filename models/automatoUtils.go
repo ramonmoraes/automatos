a2be3a5fc5ca1a723b01860f6642e04a63785eec
Ramon Moraes <ramon.moraes94@gmail.com>
2018-11-28 22:14:51 -0300

    Remove commented methods

--- models/automatoUtils.go
+++ models/automatoUtils.go
@@ -11,30 +11,3 @@ func (a *Automato) GetVariableList() []Simbol {
 	}
 	return varList
 }
-
-// // GetPossibleCreatedsSimbols should return the list of every simbol that can be created
-// func (a *Automato) GetPossibleCreatedsSimbols() []Simbol {
-// 	wordList := []Word{}
-// 	for _, expression := range a.Expressions {
-// 		wordList = append(wordList, expression.Words...)
-// 	}
-
-// 	simbolList := []Simbol{}
-// 	for _, word := range wordList {
-// 		simbolList = append(simbolList, word.Simbols...)
-// 	}
-// 	return simbolList
-// }
-
-// // GetEverySimbol should return the list of every possible variables on automato
-// func (a *Automato) GetEverySimbol() []Simbol {
-// 	return append(a.GetVariableList(), a.GetPossibleCreatedsSimbols()...)
-// }
-
-// // Explain should call explain for every expression in automato
-// func (a *Automato) Explain() {
-// 	fmt.Println("Explaining Automato:")
-// 	for _, expression := range a.Expressions {
-// 		expression.Explain()
-// 	}
-// }
